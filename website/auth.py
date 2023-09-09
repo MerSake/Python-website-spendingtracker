35696d1495d07be494747d81cabbb5cc0dab1f18
Ebrahim Fadly <96057410+MerSake@users.noreply.github.com>
2023-09-09 11:10:39 +0300

    added main 3 pages login sign up and home and linked them to the app

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("/login.html", text="testing")
 
 @auth.route('/logput')
 def logout():
-    return
+    return 0
 @auth.route('/sgin-up')
 def sign_up():
-    return
+    return render_template("/sing-up.html")
