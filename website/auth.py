0007c95faa9f1e08c6c57c13a798ac954a9842f9
Ebrahim Fadly <96057410+MerSake@users.noreply.github.com>
2023-09-10 20:00:22 +0300

    finished login, sign up page, and database

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,31 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from passlib.hash import sha256_crypt as hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['Get', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("/login.html", authorized=True)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if hash.verify(password, user.password):
+                flash('You are logged in', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('incorrect password', category='error')
+        else:
+            flash('User does not exist', category='error')
+
+    return render_template("/login.html", user=current_user)
 
 
 @auth.route('/sign_up', methods=['Get', 'POST'])
@@ -17,7 +35,10 @@ def sign_up():
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email is already used', category='error')
+        elif len(email) < 4:
             flash('Email must be greater the 3 characters.', category='error')
         elif len(firstName) < 2:
             flash('Name must be longer then 1 characters.', category='error')
@@ -26,15 +47,19 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be longer then 6 characters', category='error')
         else:
+            new_user = User(email=email, first_name=firstName,
+                            password=hash.encrypt(password1))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created', category='success')
-    return render_template("/sign_up.html")
+            return redirect(url_for('views.home'))
+
+    return render_template("/sign_up.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 0
-
-
-@auth.route('/home')
-def Home():
-    return render_template("/home.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
--- website/models.py
+++ website/models.py
@@ -11,8 +11,8 @@ class Note(db.Model):
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    first_name = db.Colmun(db.String(50))
+    password = db.Column(db.String(400))
+    first_name = db.Column(db.String(50))
     notes = db.relationship('Note')
