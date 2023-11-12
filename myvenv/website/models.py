0c516d29bc38f93ced9522c29fb00a4246e656f3
Ebrahim Fadly <96057410+MerSake@users.noreply.github.com>
2023-11-12 20:07:26 +0200

    .

--- myvenv/website/models.py
+++ myvenv/website/models.py
@@ -1,3 +1,4 @@
+import datetime
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -16,3 +17,14 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(400))
     first_name = db.Column(db.String(50))
     notes = db.relationship('Note')
+
+class Expenses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, default=datetime.date.today())
+    groceriesEx = db.Column(db.Integer, default=0)
+    ordersEx = db.Column(db.Integer, default=0)
+    HomeEx = db.Column(db.Integer, default=0)
+    restaurantEx = db.Column(db.Integer, default=0)
+    otherExpensesEx = db.Column(db.Integer, default=0)
+    notes = db.Column(db.String(500))
+    
--- myvenv/website/views.py
+++ myvenv/website/views.py
@@ -1,5 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+import datetime
+from sqlalchemy import extract
+from . import db
+from .models import Expenses
+import pygal 
+import calendar
 
 
 views = Blueprint('views', __name__)
@@ -11,7 +17,66 @@ def home():
     return render_template("/home.html", user=current_user)
 
 
-@views.route('/monthlyExpenses')
+@views.route('/monthlyExpenses', methods=['Get', 'POST'])
 @login_required
 def expenses():
-    return render_template("/monthlyExpenses.html", user=current_user)
+    
+    expenses = Expenses.query.filter(extract('month', Expenses.date) == datetime.date.today().month).all()
+    
+    
+    total_expenses = {
+    'groceriesEx': 0,
+    'ordersEx': 0,
+    'HomeEx': 0,
+    'restaurantEx': 0,
+    'otherExpensesEx': 0
+    }
+    
+    for expense in expenses:
+        total_expenses['groceriesEx'] += expense.groceriesEx
+        total_expenses['ordersEx'] += expense.ordersEx
+        total_expenses['HomeEx'] += expense.HomeEx
+        total_expenses['restaurantEx'] += expense.restaurantEx
+        total_expenses['otherExpensesEx'] += expense.otherExpensesEx
+        
+    total = total_expenses['groceriesEx']+total_expenses['ordersEx']+total_expenses['HomeEx']+total_expenses['restaurantEx']+total_expenses['otherExpensesEx'] 
+    
+    chart = pygal.Pie()
+    chart.title = "Expenses of " + calendar.month_name[datetime.date.today().month]
+    chart.add('Groceries expenses', total_expenses['groceriesEx'])
+    chart.add('Orders expenses', total_expenses['ordersEx'])
+    chart.add('Home expenses', total_expenses['HomeEx'])
+    chart.add('Restaurant expenses', total_expenses['restaurantEx'])
+    chart.add('Other expenses', total_expenses['otherExpensesEx'])
+    
+    
+    if request.method == 'POST':
+        groceries = request.form.get('groceries')
+        orders = request.form.get('orders')
+        Home = request.form.get('Home')
+        restaurant = request.form.get('restaurant')
+        otherExpenses = request.form.get('otherExpenses')
+        notes = request.form.get('notes')
+        if Expenses.query.filter_by(date=datetime.date.today()).first():
+            user = Expenses.query.filter_by(date=datetime.date.today()).first()
+            if groceries != '':
+                user.groceriesEx += int(groceries)
+            if orders != '':
+                user.ordersEx += int(orders)
+            if Home != '':
+                user.HomeEx += int(Home)
+            if restaurant != '':
+                user.restaurantEx += int(restaurant)
+            if otherExpenses != '':
+                user.otherExpensesEx += int(otherExpenses)
+            user.notes += "\n|" + notes
+            db.session.commit()
+        else:
+            new_record = Expenses(date=datetime.date.today(), groceriesEx=groceries, ordersEx=orders, HomeEx=Home, restaurantEx=restaurant, otherExpensesEx=otherExpenses, notes=notes)
+            db.session.add(new_record)
+            db.session.commit()
+        
+        return redirect(url_for('views.expenses'))
+    
+    chart = chart.render_data_uri()
+    return render_template("/monthlyExpenses.html", user=current_user, chart = chart)
